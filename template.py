d1242a10a238027544efc0586d313644765efc28
HereticInquisitor <ayushraj7353@gmail.com>
2024-03-21 23:31:25 +0530

    template

--- template.py
+++ template.py
@@ -2,42 +2,47 @@ import os
 from pathlib import Path
 import logging
 
-list_of_files =[
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-    ".github/workflows/.gitkeep",
-    "src/__init__.py",
-    "src/components/__init__.py",
-    "src/components/data_ingestion.py",
-    "src/components/data_transformation.py",
-    "src/components/model_trainer.py",
-    "src/components/model_evaluation.py",
-    "src/pipeline/__init__.py",
-    "src/pipeline/training_pipeline.py",
-    "src/pipeline/prediction_pipeline.py",
-    "src/utils/__init__.py",
-    "src/utils/utils.py",
-    "src/logger/logging.py",
-    "src/exception/exception.py",
-    "tests/unit/__init__.py",
-    "tests/integration/__init__.py",
-    "init_setup.sh",
+
+project_name = "cnnClassifier"
+
+list_of_files = [
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
     "requirements.txt",
-    "requirements_dev.txt",
     "setup.py",
-    "setup.cfg",
-    "pyproject.toml",
-    "tox.ini",
-    "experiments/experiments.ipynb"
+    "research/trials.ipynb",
+    "templates/index.html"
+
 
 ]
+
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
-    if filedir != "":
-        os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory: {filedir} for file: {filename}")
 
-    if(not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
-        with open(filepath, 'w') as f:
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
             pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
