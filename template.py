c38f63cebbe54a8d7f4dce01c1792dc8500dcfca
HereticInquisitor <ayushraj7353@gmail.com>
2024-03-21 23:38:57 +0530

    template

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "cnnClassifier"
+project_name = "Global_EV_value_prediction"
 
 list_of_files = [
     f"src/{project_name}/__init__.py",
