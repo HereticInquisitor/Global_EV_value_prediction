8639634add510d7b880beee7291db242ab1bf402
HereticInquisitor <ayushraj7353@gmail.com>
2024-04-04 09:58:46 +0530

    deploy

--- app.py
+++ app.py
@@ -1,17 +1,261 @@
 from flask import *
 import os
 import requests
+import joblib
+
+import numpy as np
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template()
+    return render_template("main.html")
 
-@app.route('/submit',methods=["POST","GET"])
+# def verify1(input):
+
+
+@app.route('/submit',methods=["POST"])
 def page1():
+    input=[]
     if request.method == "POST":
-        pass
-    else:
-        return render_template('error')
-    
+        data = request.json
+        region = data.get("region")
+        input.append(region)
+        category = data.get("category")
+        input.append(category)
+        parameter = data.get("parameter")
+        input.append(parameter)
+        mode = data.get("mode")
+        input.append(mode)
+        powertrain = data.get("powertrain")
+        input.append(powertrain)
+        year = data.get("year")
+        input.append(year)
+        unit = data.get("unit")
+        input.append(unit)
+
+    # Process the data as needed
+        print(region, category, parameter, mode, powertrain, year, unit)
+        # input= [request.form.get("region"),
+        #         request.form.get("category"),
+        #         request.form.get("parameter"),
+        #         request.form.get("mode"),
+        #         request.form.get("powertrain"),
+        #         request.form.get("year"),
+        #         request.form.get("unit")
+        #         ]
+        print(input)
+        for i in input:
+            if len(i)==0:
+                return render_template('submit.html')
+        try:
+            if input[6] == 'Vehicles':
+                # if verify1(input) ==False:
+                #     return render_template("error.html")
+                model= joblib.load('model/model1.pkl')
+                X_scaler = joblib.load('scaler/X_scaler1.pkl')
+                Y_scaler = joblib.load('scaler/Y_scaler1.pkl')
+                encoder= joblib.load('encoder/encoder1.pkl')
+                print(input)
+                input = np.array(input)
+                input= input.reshape(1,-1)
+                input_f=input[:,:-2]
+                input_s= input[:,-1]
+                input_sp= input[:,-2]
+                input_s= np.reshape(input_s, (-1,1))
+                input_sp = np.reshape(input_sp, (-1,1))
+                input_merge= np.concatenate((input_f, input_s), axis=1)
+                input_enc = encoder.transform(input_merge)
+                input_enc_merge= np.concatenate((input_enc, input_sp), axis=1)
+                input_scaled= X_scaler.transform(input_enc_merge)
+                result_scaled= model.predict(input_scaled)
+                result= result_scaled.reshape(1,-1)
+                result=Y_scaler.inverse_transform(result)
+                result= result.reshape(-1,1)
+                print("Result",result)
+                # result = {'result':result}
+                print(result)
+                # result = result.tolist()
+                json_result= json.dumps(result)
+                return render_template("main.html", value_pred=12345)
+                # return jsonify(result)
+                # print(result)
+                    # print("error")
+
+            elif input[6] == 'percent':
+                # if verify2(input) ==False:
+                #     return render_template("error.html")
+                # input[5]=int(input[5])
+                model= joblib.load('model/model2.pkl')
+                X_scaler = joblib.load('scaler/X_scaler2.pkl')
+                Y_scaler = joblib.load('scaler/Y_scaler2.pkl')
+                encoder= joblib.load('encoder/encoder2.pkl')
+                print(input)
+                input = np.array(input)
+                input= input.reshape(1,-1)
+                input_f=input[:,:-2]
+                input_s= input[:,-1]
+                input_sp= input[:,-2]
+                input_s= np.reshape(input_s, (-1,1))
+                input_sp = np.reshape(input_sp, (-1,1))
+                input_merge= np.concatenate((input_f, input_s), axis=1)
+                input_enc = encoder.transform(input_merge)
+                input_enc_merge= np.concatenate((input_enc, input_sp), axis=1)
+                input_scaled= X_scaler.transform(input_enc_merge)
+                result_scaled= model.predict(input_scaled)
+                result= result_scaled.reshape(1,-1)
+                result=Y_scaler.inverse_transform(result)
+                result= result.reshape(-1,1)
+                print("Result",result)
+                # result = {'result':result}
+                print(result)
+                result = result.tolist()
+                json_result= json.dumps(result)
+                # return render_template("main.html", data=result)
+                return jsonify(result)
+                
+                # print(result)
+
+            elif input[6] == 'charging points':
+                # if verify3(input) ==False:
+                #     return render_template("error.html")
+                model= joblib.load('model/model3.pkl')
+                X_scaler = joblib.load('scaler/X_scaler3.pkl')
+                Y_scaler = joblib.load('scaler/Y_scaler3.pkl')
+                encoder= joblib.load('encoder/encoder3.pkl')
+                print(input)
+                input = np.array(input)
+                input= input.reshape(1,-1)
+                input_f=input[:,:-2]
+                input_s= input[:,-1]
+                input_sp= input[:,-2]
+                input_s= np.reshape(input_s, (-1,1))
+                input_sp = np.reshape(input_sp, (-1,1))
+                input_merge= np.concatenate((input_f, input_s), axis=1)
+                input_enc = encoder.transform(input_merge)
+                input_enc_merge= np.concatenate((input_enc, input_sp), axis=1)
+                input_scaled= X_scaler.transform(input_enc_merge)
+                result_scaled= model.predict(input_scaled)
+                result= result_scaled.reshape(1,-1)
+                result=Y_scaler.inverse_transform(result)
+                result= result.reshape(-1,1)
+                print("Result",result)
+                # result = {'result':result}
+                print(result)
+                result = result.tolist()
+                json_result= json.dumps(result)
+                # return render_template("main.html", data=result)
+                return jsonify(result)
+                
+                # print(result)
+
+
+            elif input[6] == 'GWh':
+                # if verify4(input) ==False:
+                #     return render_template("error.html")
+                model= joblib.load('model/model4.pkl')
+                X_scaler = joblib.load('scaler/X_scaler4.pkl')
+                Y_scaler = joblib.load('scaler/Y_scaler4.pkl')
+                encoder= joblib.load('encoder/encoder4.pkl')
+                print(input)
+                input = np.array(input)
+                input= input.reshape(1,-1)
+                input_f=input[:,:-2]
+                input_s= input[:,-1]
+                input_sp= input[:,-2]
+                input_s= np.reshape(input_s, (-1,1))
+                input_sp = np.reshape(input_sp, (-1,1))
+                input_merge= np.concatenate((input_f, input_s), axis=1)
+                input_enc = encoder.transform(input_merge)
+                input_enc_merge= np.concatenate((input_enc, input_sp), axis=1)
+                input_scaled= X_scaler.transform(input_enc_merge)
+                result_scaled= model.predict(input_scaled)
+                result= result_scaled.reshape(1,-1)
+                result=Y_scaler.inverse_transform(result)
+                result= result.reshape(-1,1)
+                print("Result",result)
+                # result = {'result':result}
+                print(result)
+                result = result.tolist()
+                json_result= json.dumps(result)
+                # return render_template("main.html", data=result)
+                return jsonify(result)
+                
+                # print(result)
+
+            elif input[6] == 'Milion barrels per day':
+                # if verify5(input) ==False:
+                #     return render_template("error.html")
+                model= joblib.load('model/model5.pkl')
+                X_scaler = joblib.load('scaler/X_scaler5.pkl')
+                Y_scaler = joblib.load('scaler/Y_scaler5.pkl')
+                encoder= joblib.load('encoder/encoder5.pkl')
+                print(input)
+                input = np.array(input)
+                input= input.reshape(1,-1)
+                input_f=input[:,:-2]
+                input_s= input[:,-1]
+                input_sp= input[:,-2]
+                input_s= np.reshape(input_s, (-1,1))
+                input_sp = np.reshape(input_sp, (-1,1))
+                input_merge= np.concatenate((input_f, input_s), axis=1)
+                input_enc = encoder.transform(input_merge)
+                input_enc_merge= np.concatenate((input_enc, input_sp), axis=1)
+                input_scaled= X_scaler.transform(input_enc_merge)
+                result_scaled= model.predict(input_scaled)
+                result= result_scaled.reshape(1,-1)
+                result=Y_scaler.inverse_transform(result)
+                result= result.reshape(-1,1)
+                print("Result",result)
+                # result = {'result':result}
+                print(result)
+                result = result.tolist()
+                json_result= json.dumps(result)
+                # return render_template("main.html", data=result)
+                return jsonify(result)
+                
+                # print(result)
+
+
+            elif input[6] == 'Oil displacement, million lge':
+                # if verify6(input) ==False:
+                #     return render_template("error.html")
+                model= joblib.load('mmodel/odel6.pkl')
+                X_scaler = joblib.load('scaler/X_scaler6.pkl')
+                Y_scaler = joblib.load('scaler/Y_scaler6.pkl')
+                encoder= joblib.load('encoder/encoder6.pkl')
+                print(input)
+                input = np.array(input)
+                input= input.reshape(1,-1)
+                input_f=input[:,:-2]
+                input_s= input[:,-1]
+                input_sp= input[:,-2]
+                input_s= np.reshape(input_s, (-1,1))
+                input_sp = np.reshape(input_sp, (-1,1))
+                input_merge= np.concatenate((input_f, input_s), axis=1)
+                input_enc = encoder.transform(input_merge)
+                input_enc_merge= np.concatenate((input_enc, input_sp), axis=1)
+                input_scaled= X_scaler.transform(input_enc_merge)
+                result_scaled= model.predict(input_scaled)
+                result= result_scaled.reshape(1,-1)
+                result=Y_scaler.inverse_transform(result)
+                result= result.reshape(-1,1)
+                print("Result",result)
+                # result = {'result':result}
+                print(result)
+                result = result.tolist()
+                json_result= json.dumps(result)
+                # return render_template("main.html", data=result)
+                return jsonify(result)
+                
+                # print(result)
+            
+            else:
+                return render_template('error')
+        
+        except:
+            return "error"
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/index.js
@@ -0,0 +1,178 @@
+!(function ($) {
+  "use strict";
+
+  // Nav Menu
+  $(document).on(
+    "click",
+    ".nav-menu a, .mobile-nav a, .social-links a",
+    function (e) {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var hash = this.hash;
+        var target = $(hash);
+        if (target.length) {
+          e.preventDefault();
+
+          if ($(this).parents(".nav-menu, .mobile-nav").length) {
+            $(".nav-menu .active, .mobile-nav .active").removeClass("active");
+            $(this).closest("li").addClass("active");
+          }
+
+          if (hash == "#header") {
+            $("#header").removeClass("header-top");
+            $("section").removeClass("section-show");
+            if ($("body").hasClass("mobile-nav-active")) {
+              $("body").removeClass("mobile-nav-active");
+              $(".mobile-nav-toggle i").toggleClass(
+                "icofont-navigation-menu icofont-close"
+              );
+              $(".mobile-nav-overly").fadeOut();
+            }
+            return;
+          }
+
+          if (!$("#header").hasClass("header-top")) {
+            $("#header").addClass("header-top");
+            setTimeout(function () {
+              $("section").removeClass("section-show");
+              $(hash).addClass("section-show");
+            }, 350);
+          } else {
+            $("section").removeClass("section-show");
+            $(hash).addClass("section-show");
+          }
+
+          $("html, body").animate(
+            {
+              scrollTop: 0,
+            },
+            350
+          );
+
+          if ($("body").hasClass("mobile-nav-active")) {
+            $("body").removeClass("mobile-nav-active");
+            $(".mobile-nav-toggle i").toggleClass(
+              "icofont-navigation-menu icofont-close"
+            );
+            $(".mobile-nav-overly").fadeOut();
+          }
+
+          return false;
+        }
+      }
+    }
+  );
+
+  // Activate/show sections on load with hash links
+  if (window.location.hash) {
+    var initial_nav = window.location.hash;
+    if ($(initial_nav).length) {
+      $("#header").addClass("header-top");
+      $(".nav-menu .active, .mobile-nav .active").removeClass("active");
+      $(".nav-menu, .mobile-nav")
+        .find('a[href="' + initial_nav + '"]')
+        .parent("li")
+        .addClass("active");
+      setTimeout(function () {
+        $("section").removeClass("section-show");
+        $(initial_nav).addClass("section-show");
+      }, 350);
+    }
+  }
+
+  // $(document).ready(function () {
+  //   $(".form").submit(function (event) {
+  //     event.preventDefault();
+  //     var selectedSection = $("#valuePred")[0];
+  //     selectedSection.scrollIntoView({ behavior: "smooth" });
+  //   });
+  // });
+
+  //Getting the name for prediction_page
+  $(document).ready(function () {
+    $("#btn").click(function () {
+      var userName = $("#inputValue").val();
+      $("#username").html("Welcome " + userName);
+    });
+  });
+
+  //Handling behaviour of form
+  var i = 0;
+  $(document).ready(function () {
+    $("select").change(function () {
+      const selectedValue = $(this).val(); // Get the selected value
+      const fieldName = $(this).attr("name"); // Get the name of the select field
+      const modifiedText =
+        fieldName.charAt(0).toUpperCase() + fieldName.slice(1);
+      i++;
+      $(`.${fieldName}op`).removeClass(`${fieldName}op`);
+      // Display the selected value
+      $(`#${fieldName}img`).attr(
+        "src",
+        `static/img/${fieldName}/${selectedValue}.png`
+      );
+      $(`#${fieldName}h3`).html(`${modifiedText}: ${selectedValue}`);
+    });
+  });
+
+  // Mobile Navigation
+  if ($(".nav-menu").length) {
+    var $mobile_nav = $(".nav-menu").clone().prop({
+      class: "mobile-nav d-lg-none",
+    });
+    $("body").append($mobile_nav);
+    $("body").prepend(
+      '<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>'
+    );
+    $("body").append('<div class="mobile-nav-overly"></div>');
+
+    $(document).on("click", ".mobile-nav-toggle", function (e) {
+      $("body").toggleClass("mobile-nav-active");
+      $(".mobile-nav-toggle i").toggleClass(
+        "icofont-navigation-menu icofont-close"
+      );
+      $(".mobile-nav-overly").toggle();
+    });
+
+    $(document).click(function (e) {
+      var container = $(".mobile-nav, .mobile-nav-toggle");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($("body").hasClass("mobile-nav-active")) {
+          $("body").removeClass("mobile-nav-active");
+          $(".mobile-nav-toggle i").toggleClass(
+            "icofont-navigation-menu icofont-close"
+          );
+          $(".mobile-nav-overly").fadeOut();
+        }
+      }
+    });
+  } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
+    $(".mobile-nav, .mobile-nav-toggle").hide();
+  }
+
+  // jQuery counterUp
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 1000,
+  });
+
+  $(".testimonials-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      900: {
+        items: 3,
+      },
+    },
+  });
+})(jQuery);
